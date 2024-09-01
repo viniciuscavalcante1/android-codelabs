872c487888f9b31d4198e9fd9f4ed07b5a2b48ce
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-09-01 19:39:49 -0300

    add: more functions

--- functions.kt
+++ functions.kt
@@ -10,8 +10,42 @@ fun displayHello() {
     println("Hello, world!")
 }
 
+fun displayHelloAndroidTwice() {
+    println("Hello, Android!")
+    println("Hello, Android!")
+}
+
+fun displayGreetings(name: String) {
+    println("Hello, $name!")
+}
+
+fun displayOneToThree() {
+    println("1")
+    println("2")
+    println("3")
+}
+
+fun displayLearning() {
+    println("I'm")
+    println("Learning")
+    println("Kotlin!")
+}
+
+fun displayWeekdays() {
+    println("Monday")
+    println("Tuesday")
+    println("Wednesday")
+    println("Thursday")
+    println("Friday")
+}
+
 fun main() {
     println(addOne(1))
     printFullName("Vinícius", "Cavalcante")
     displayHello()
+    displayHelloAndroidTwice()
+    displayGreetings("Vinícius")
+    displayOneToThree()
+    displayLearning()
+    displayWeekdays()
 }
