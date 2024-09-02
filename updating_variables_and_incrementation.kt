71a363736a34a938dc7c95bf93c084ba9f27bc71
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-09-01 21:48:46 -0300

    add: increment, decrement and var

---
+++ updating_variables_and_incrementation.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var cartTotal: Int = 20
+    println("Total: $cartTotal")
+
+    cartTotal++
+    println("Total: $cartTotal")
+
+    cartTotal--
+    cartTotal--
+    println("Total: $cartTotal")
+}
