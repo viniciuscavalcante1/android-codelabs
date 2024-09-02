c691d6c2cb8bd69440a1c36eeeb212a895f69a90
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-09-01 21:57:27 -0300

    add: string data type, concatenation and escape sequences

---
+++ string_data_type.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val nextMeeting = "Next meeting: "
+    val date = "january 1 "
+    val location = "at work"
+    val reminder = nextMeeting + date + location
+    println(reminder)
+
+    val escapeSequence = "Say \"hello\""
+    println(escapeSequence)
+}
