8edaad7b228cb08086691c6a7e9679041e617eaa
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-09-01 21:50:05 -0300

    add: double data type

---
+++ double_data_type.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val trip1 = 3.20
+    val trip2 = 4.10
+    val trip3 = 1.34
+    val totalTripLength = trip1 + trip2 + trip3
+    println("$totalTripLength kilometers left to destination")
+}
