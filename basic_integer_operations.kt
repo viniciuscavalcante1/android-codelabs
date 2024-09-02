a71c2aca00d7547e3bddd5e8b387b35252aa194f
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-09-01 21:27:28 -0300

    add: basic integer operations

---
+++ basic_integer_operations.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val unreadCount = 100
+    val readCount = 10
+    println("You have ${unreadCount + readCount} total messages in your inbox.")
+
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos.")
+    println("$photosDeleted photos deleted.")
+    println("${numberOfPhotos - photosDeleted} photos left.")
+}
